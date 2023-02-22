543e66780f1b5a767b76e16bb4381f2f92c7b366
lucasvm01 <lucasvm01@gmail.com>
2023-02-22 19:08:54 -0300

    Added manual multiple rendering

--- OpenGLLL/src/Main.cpp
+++ OpenGLLL/src/Main.cpp
@@ -62,10 +62,10 @@ int main(void) {
 	// Setting up entities
 		// Vertices
 		float pos[] = {
-			100.0f, 100.0f, 0.0f, 0.0f,
-			200.0f, 100.0f, 1.0f, 0.0f,
-			200.0f, 200.0f, 1.0f, 1.0f,
-			100.0f, 200.0f, 0.0f, 1.0f
+			-50.0f, -50.0f, 0.0f, 0.0f,
+			 50.0f, -50.0f, 1.0f, 0.0f,
+			 50.0f,  50.0f, 1.0f, 1.0f,
+			-50.0f,  50.0f, 0.0f, 1.0f
 		};
 
 		// Indices of the positions in order of drawing
@@ -73,6 +73,9 @@ int main(void) {
 			0, 1, 2,
 			2, 3, 0
 		};
+
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
 		// VAs, VBs and IBs
 		VertexArray VA;
@@ -85,26 +88,25 @@ int main(void) {
 
 		IndexBuffer IB(indices, 6);
 
+		// Setup projection matrix
+		glm::mat4 proj = glm::ortho(0.0f, 640.0f, 0.0f, 480.0f, -1.0f, 1.0f);
+		// Setup view matrix
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+
 		// Setup shaders
 		Shader shader(SHADERS_FILE_PATH);
 		shader.Bind();
-
-		// Setup projection matrix
-		glm::mat4 proj = glm::ortho(0.0f, 640.0f, 0.0f	, 480.0f, -1.0f, 1.0f);
-		// Setup view matrix
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-		// Setup model matrix
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-		// Set mvp matrix
-		glm::mat4 mvp = proj * view * model;
-		shader.SetUniformMat4f("u_mvp", mvp);
 
 		// Setup textures
 		Texture texture(TEXTURES_FILE_PATH);
 		texture.Bind();
 
 		shader.SetUniform1i("u_texture", 0);
+
+		VA.Unbind();
+		VB.Unbind();
+		IB.Unbind();
+		shader.Unbind();
 
 		// Setup Renderer
 		Renderer renderer;
@@ -117,12 +119,16 @@ int main(void) {
 		ImGui_ImplOpenGL3_Init(glsl_version);
 		ImGui::StyleColorsDark();
 
-		// Setup logic
-
 		// ImGui
 		bool show_demo_window = true;
 		bool show_another_window = false;
 		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+		// Setup logic
+		
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
 
 		// Main loop
 		while (!glfwWindowShouldClose(window)) {
@@ -136,32 +142,39 @@ int main(void) {
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
 
-			// Draw call
-			renderer.Draw(VA, IB, shader);
+			shader.Bind();
+
+			{
+				// Setup model matrix
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+
+				// Set mvp matrix
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_mvp", mvp);
+
+				// Draw call
+				renderer.Draw(VA, IB, shader);
+			}
+
+			{
+				// Setup model matrix
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+
+				// Set mvp matrix
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_mvp", mvp);
+
+				// Draw call
+				renderer.Draw(VA, IB, shader);
+			}
 
 			// Loop logic
 
 			// Simple window ImGui
 			{
-				static float f = 0.0f;
-				static int counter = 0;
-
-				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-				ImGui::Checkbox("Another Window", &show_another_window);
-
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-				ImGui::End();
+				ImGui::SliderFloat3("Translation A: ", &translationA.x, 0.0f, 960.0f);
+				ImGui::SliderFloat3("Translation B: ", &translationB.x, 0.0f, 960.0f);
+				ImGui::Text("AVG Framerate: ¨.3f ms/f (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			}
 
 			// Rendering ImGui
