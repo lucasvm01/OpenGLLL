35080dd4f32e777a6947e2a83a5ce349e4e34961
lucasvm01 <lucasvm01@gmail.com>
2023-03-09 03:05:00 -0300

    cleaned main file

--- OpenGLLL/src/Main.cpp
+++ OpenGLLL/src/Main.cpp
@@ -60,53 +60,7 @@ int main(void) {
 
 	{
 	// Setting up entities
-		// Vertices
-		float pos[] = {
-			-50.0f, -50.0f, 0.0f, 0.0f,
-			 50.0f, -50.0f, 1.0f, 0.0f,
-			 50.0f,  50.0f, 1.0f, 1.0f,
-			-50.0f,  50.0f, 0.0f, 1.0f
-		};
-
-		// Indices of the positions in order of drawing
-		unsigned int indices[] = {
-			0, 1, 2,
-			2, 3, 0
-		};
-
-		GLCall(glEnable(GL_BLEND));
-		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-		// VAs, VBs and IBs
-		VertexArray VA;
-		VertexBuffer VB(pos, 4 * 4 * sizeof(float));
-
-		VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		VA.AddBuffer(VB, layout);
-
-		IndexBuffer IB(indices, 6);
-
-		// Setup projection matrix
-		glm::mat4 proj = glm::ortho(0.0f, 640.0f, 0.0f, 480.0f, -1.0f, 1.0f);
-		// Setup view matrix
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-		// Setup shaders
-		Shader shader(SHADERS_FILE_PATH);
-		shader.Bind();
-
-		// Setup textures
-		Texture texture(TEXTURES_FILE_PATH);
-		texture.Bind();
-
-		shader.SetUniform1i("u_texture", 0);
-
-		VA.Unbind();
-		VB.Unbind();
-		IB.Unbind();
-		shader.Unbind();
+		
 
 		// Setup Renderer
 		Renderer renderer;
@@ -125,9 +79,6 @@ int main(void) {
 		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 		// Setup logic
-		
-		glm::vec3 translationA(200, 200, 0);
-		glm::vec3 translationB(400, 200, 0);
 
 		// Main loop
 		while (!glfwWindowShouldClose(window)) {
@@ -143,38 +94,6 @@ int main(void) {
 
 			// Loop logic
 
-			shader.Bind();
-
-			{
-				// Setup model matrix
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-
-				// Set mvp matrix
-				glm::mat4 mvp = proj * view * model;
-				shader.SetUniformMat4f("u_mvp", mvp);
-
-				// Draw call
-				renderer.Draw(VA, IB, shader);
-			}
-
-			{
-				// Setup model matrix
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-
-				// Set mvp matrix
-				glm::mat4 mvp = proj * view * model;
-				shader.SetUniformMat4f("u_mvp", mvp);
-
-				// Draw call
-				renderer.Draw(VA, IB, shader);
-			}
-
-			// Simple window ImGui
-			{
-				ImGui::SliderFloat3("Translation A: ", &translationA.x, 0.0f, 960.0f);
-				ImGui::SliderFloat3("Translation B: ", &translationB.x, 0.0f, 960.0f);
-				ImGui::Text("AVG Framerate: ¨.3f ms/f (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			}
 
 			// Rendering ImGui
 			ImGui::Render();
--- OpenGLLL/src/test/Test.h
+++ OpenGLLL/src/test/Test.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include<functional>
+
 namespace test {
 	class Test {
 	public:
@@ -9,5 +11,16 @@ namespace test {
 		virtual void OnUpdate(float deltaTime){}
 		virtual void OnRender(){}
 		virtual void OnImGuiRender(){}
+	};
+
+	class TestMenu : public Test {
+	public:
+		TestMenu();
+		~TestMenu();
+
+		virtual void OnImGuiRender() override;
+	private:
+		Test* m_current_test;
+		std::vector<std::pair<std::string, std::function<Test* ()>>> m_tests;
 	};
 }
