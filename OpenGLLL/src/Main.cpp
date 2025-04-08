bff505ada6566ee47fea8bf5b74bdcc7958276b3
lucasvm01 <lucasvm01@gmail.com>
2025-04-08 14:13:58 -0300

    Fixed compiler issues

--- OpenGLLL/src/Main.cpp
+++ OpenGLLL/src/Main.cpp
@@ -10,7 +10,7 @@
 
 #include"imgui/imgui.h"
 #include"imgui/imgui_impl_glfw.h"
-#include"imgui/imgui_impl_opengl3.h"
+#include"imgui/imgui_impl_opengl3.h" 
 
 #include"test/Test.h"
 #include"test/ClearColor/TestClearColor.h"
